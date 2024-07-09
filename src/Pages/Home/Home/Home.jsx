b3494394fd3f3ad66ff1902405ba2cadf6384152
Md Yeasin <mdyeasinhn@gmail.com>
2024-07-09 20:03:14 +0600

    home page updated

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            {/* <TopFoods></TopFoods> */}
+            <TopFoods></TopFoods>
         </div>
     );
 };
